7deb9ee87d5acc9e10f23da3ff6cff8d031eeb1f
KirillSavoshinskiy <savosh28@gmail.com>
2023-10-08 18:55:39 +0300

    added: modules importing

--- PaymentClient/src/app/app.module.ts
+++ PaymentClient/src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PaymentSystemsListComponent } from './components/payment-systems-list/payment-systems-list.component';
+import { PaymentCreationComponent } from './components/payment-creation/payment-creation.component';
+import { PaymentsListComponent } from './components/payments-list/payments-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentSystemsListComponent,
+    PaymentCreationComponent,
+    PaymentsListComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzGridModule,
+    NzButtonModule,
+    NzFormModule,
+    NzSelectModule,
+    NzCardModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
