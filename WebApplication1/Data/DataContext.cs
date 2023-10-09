824b1c51d7713272393c340bbb57735df1bc7372
KirillSavoshinskiy <savosh28@gmail.com>
2023-10-09 09:46:08 +0300

    added: DataContext and seed stuff

---
+++ WebApplication1/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<PaymentSystem> PaymentSystems { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Payment>().HasOne(c => c.Currency).WithMany(p => p.Payments);
+            builder.Entity<Payment>().HasOne(p => p.PaymentSystem).WithMany(p => p.Payments);
+            builder.Entity<PaymentSystem>().HasMany(c => c.Currencies).WithMany(p => p.PaymentSystems);
+            builder.Entity<PaymentSystem>().HasMany(c => c.Payments).WithOne(p => p.PaymentSystem);
+        }
+    }
+}
---
+++ WebApplication1/Data/Seed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class Seed
+    {
+        public static async Task SeedInitialDataAsync(DataContext context)
+        {
+            if (await context.PaymentSystems.AnyAsync()) { return; }
+
+            var pSystems = new List<PaymentSystem>
+            {
+                new PaymentSystem { Name = "PayPal", Description = "Description" },
+                new PaymentSystem { Name = "Binance", Description = "Description" },
+                new PaymentSystem { Name = "WebMoney", Description = "Description" }
+            };
+
+            await context.AddRangeAsync(pSystems);
+
+            var currencies = new List<Currency> {
+                new Currency { Name = "USD", IsCrypto = false },
+                new Currency { Name = "EUR", IsCrypto = false },
+                new Currency { Name = "BYN", IsCrypto = false },
+                new Currency { Name = "BIT", IsCrypto = true },
+                new Currency { Name = "USDT", IsCrypto = true }
+            };
+
+            await context.AddRangeAsync(currencies);
+
+            var payments = new List<Payment>
+            {
+                new Payment { Name = "Payment1", Amount = 120, Status = "Active", Description = "Description", Currency = currencies[0]},
+                new Payment { Name = "Payment2", Amount = 20, Status = "In progress", Description = "Description" , Currency = currencies[1]},
+                new Payment { Name = "Payment3", Amount = 320, Status = "Active", Description = "Description" , Currency = currencies[2]},
+                new Payment { Name = "Payment4", Amount = 550, Status = "Finished", Description = "Description" , Currency = currencies[0]}
+            }; 
+
+            pSystems[0].Currencies = new List<Currency> { currencies[0], currencies[1], currencies[2] };
+            pSystems[0].Payments = payments;
+            pSystems[1].Currencies = new List<Currency> { currencies[3], currencies[4] };
+            pSystems[2].Currencies = new List<Currency> { currencies[0], currencies[1], currencies[4], currencies[3] };
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
